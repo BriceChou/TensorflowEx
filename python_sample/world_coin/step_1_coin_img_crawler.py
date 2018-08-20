355113b709d1da6e1d408648e85a30aae25b4bdd
Mingliang ZHOU <bricechou@gmail.com>
2018-08-20 22:58:43 +0800

    add logger to record the running log

--- python_sample/world_coin/step_1_coin_img_crawler.py
+++ python_sample/world_coin/step_1_coin_img_crawler.py
@@ -2,28 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import os
-from requests_html import HTML, HTMLSession
+from requests_html import HTMLSession
+from logger import Logger
 import warnings
 import traceback
 
 warnings.filterwarnings('ignore')
 session = HTMLSession()
 
+with open('debug.log', 'w') as sf:
+    sf.write('-*-*========== All start to Running ... ==========*-*-\n')
+
+# create log file in current folder
+log = Logger('debug.log', level='debug')
+
 
 def download(url, filepath, backup_url=None):
     if os.path.exists(filepath):
-        print('This file {} aready existed.'.format(filepath))
+        log.logger.error('This file {} aready existed.'.format(filepath))
         return
     try:
         r = session.get(url, stream=True, timeout=60)
-        if r.status_code == 200:
-            r.raise_for_status()
+        log.logger.debug('Website URL status code is {}'.format(r.status_code))
+        if 200 == r.status_code:
+            log.logger.debug('Start to save file {}'.format(filepath))
             with open(filepath, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=1024):
-                    if chunk:  # filter out keep-alive new chunks
+                    # filter out keep-alive new chunks
+                    if chunk:
                         f.write(chunk)
                         f.flush()
-            print('{} has been saved.'.format(filepath))
+            log.logger.debug('{} saved.'.format(filepath))
             return filepath
         else:
             download(backup_url, filepath)
@@ -45,22 +54,30 @@ def download_single_country_coin_img(
     big_img_base_url = '{}wcg/img_hr/'.format(backup_url)
     country_link = '{}_all'.format(country_link_name)
     url = '{}{}.php'.format(small_img_base_url, country_link)
-    comma = ','
-    dot = '.'
 
     try:
-        html = HTML(html=url, url='bunk', default_encoding='utf-8')
-        img_list = html.find('td > img')
-        text_list = html.find('img + br')
-        for (img, text) in zip(img_list, text_list):
-            src_url = img.attrs.get('src')
-            img_url = big_img_base_url + src_url
-            backup_img_url = small_img_base_url + src_url
-            img_name = text.text.replace(comma, '').replace(dot, '').replace(' ', '_')
-            img_type = img.attrs.get('src').split('.')[-1]
-            img_path = '{}/{}.{}'.format(img_folder_path, img_name, img_type)
-            download(img_url, img_path, backup_img_url)
-
+        log.logger.debug('Start to load website {}'.format(url))
+        r = session.get(url, stream=True, timeout=60)
+        if 200 == r.status_code:
+            img_list = r.html.find('td > img')
+            text_list = r.html.find('img + br')
+            log.logger.info('image list size is {}'.format(len(img_list)))
+            for (img, text) in zip(img_list, text_list):
+                src_url = img.attrs.get('src')
+                img_url = big_img_base_url + src_url
+                backup_img_url = small_img_base_url + src_url
+                img_name = (
+                    text.text.replace(',', '')
+                    .replace('.', '')
+                    .replace('/', 'divided_by')
+                    .replace(' ', '_')
+                )
+                img_type = img.attrs.get('src').split('.')[-1]
+                img_path = '{}/{}.{}'.format(img_folder_path, img_name, img_type)
+                log.logger.debug('Start to download image URL: {}'.format(img_url))
+                download(img_url, img_path, backup_img_url)
+        else:
+            log.logger.error('Failed to open URL: {}.\n'.format(url))
     except KeyError:
         raise ValueError(f'Oops! There is no any images and text.')
 
@@ -75,16 +92,19 @@ if os.path.exists(main_folder_name) is False:
     os.makedirs(main_folder_name)
 
 try:
-    html = HTML(html=country_list_url, url='bunk', default_encoding='utf-8')
-    country_link_list = html.find('b > a')
-    for item in country_link_list:
-        country_link_name = item.attrs.get('href').replace('/', '.').split('.')[-2]
-        img_folder_path = os.path.join(main_folder_name, country_link_name)
-        if os.path.exists(img_folder_path) is False:
-            os.makedirs(img_folder_path)
-        download_single_country_coin_img(
-            base_url, backup_base_url, country_link_name, img_folder_path
-        )
-
+    r = session.get(country_list_url, stream=True, timeout=60)
+    if 200 == r.status_code:
+        country_link_list = r.html.find('b > a')
+        log.logger.info('country link list size is {}'.format(len(country_link_list)))
+        for item in country_link_list:
+            country_link_name = item.attrs.get('href').replace('/', '.').split('.')[-2]
+            img_folder_path = os.path.join(main_folder_name, country_link_name)
+            if os.path.exists(img_folder_path) is False:
+                os.makedirs(img_folder_path)
+            download_single_country_coin_img(
+                base_url, backup_base_url, country_link_name, img_folder_path
+            )
+    else:
+        log.logger.error('Failed to open URL: {}'.format(country_list_url))
 except KeyError:
     raise ValueError(f'Oops! There is no any country.')
